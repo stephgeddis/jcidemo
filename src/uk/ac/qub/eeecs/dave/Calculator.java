022194ecac76f46bc43a333854675cb870f3b690
40210068 <sgeddis06@qub.ac.uk>
2021-02-08 11:07:23 +0000

    new functions

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -1,12 +1,21 @@
 package uk.ac.qub.eeecs.dave;
 
 public class Calculator {
+	
 	public int add(int a, int b) {
 		return a+b;
 	}
 	
 	public int sub(int a, int b) {
 		return a-b;
+	}
+	
+	public int div(int a, int b) {
+		return a/b;
+	}
+	
+	public int mul(int a, int b) {
+		return a*b;
 	}
 	
 }
